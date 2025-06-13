d4892b7ce9601fdff9cef6e3a28358d064779042
Rui Machado <rdcsmachado@gmail.com>
2025-06-13 23:16:46 +0200

    initial support for csv

--- tap_file/client.py
+++ tap_file/client.py
@@ -2,36 +2,106 @@
 
 from __future__ import annotations
 
+from os import PathLike
 import typing as t
-
+from azure.storage.blob import BlobServiceClient
+from singer_sdk import Tap
+from singer_sdk.singerlib.schema import Schema
 from singer_sdk.streams import Stream
+from singer_sdk import typing as th 
+from smart_open import open
+import pandas as pd
+from io import StringIO
 
 if t.TYPE_CHECKING:
     from singer_sdk.helpers.types import Context
 
+class File:
 
-class FileStream(Stream):
+    def __init__(self, file_path: str, encoding: str = 'utf8', transport_params = None) -> None:
+        self.file_path = file_path
+        self.transport_params = transport_params
+        self.fd = None
+        self.encoding = encoding
+        
+    def peek(self, n_rows: int = 1000) -> list[str]:
+        rows = []
+        for i in range(n_rows):
+            rows.append(self.readline())
+        self.fd.seek(0)
+        return rows
+
+    def readline(self):
+        if not self.fd:
+            self._open_file()
+    
+        return self.fd.readline()
+
+    def _open_file(self):
+        self.fd = open(self.file_path, encoding=self.encoding, transport_params=self.transport_params)
+
+
+class CSVStream(Stream):
     """Stream class for File streams."""
 
+    def __init__(self, *args, **kwargs) -> None:
+        self.file_config = kwargs.pop("file_config")
+        self.provider_config = kwargs.pop("provider_config")
+        self.transport_params = self._build_transport_params()
+        self.header = None
+        self._schema = dict()
+        self.file = File(self.file_config['url'], transport_params=self.transport_params)
+
+        super().__init__(*args, **kwargs)
+    
+    def _build_transport_params(self):
+        t_params = {}
+        match self.provider_config['name']:
+            case 'azureblobstorage':
+                t_params['client'] = BlobServiceClient.from_connection_string(self.provider_config['azureblobstorage_connection_string'])
+        
+        return t_params
+                
+    @property
+    def schema(self):
+
+        if not self._schema:
+
+            properties: list[th.Property] = []
+
+            rows = self.file.peek()
+            df = pd.read_csv(StringIO(''.join(rows)))
+            schema = pd.io.json.build_table_schema(df, index=False)
+            type_translation = {
+                'integer': th.IntegerType(),
+                'string': th.StringType(),
+                'boolean': th.BooleanType(),
+                'number': th.NumberType(),
+                'datetime': th.DateTimeType(),
+                'duration': th.DurationType(),
+            }
+            
+            for field in schema['fields']:
+                properties.append(th.Property(field['name'], type_translation[field['type']]))
+
+            # cache header
+            self.header = [f['name'] for f in schema['fields']]
+
+            # cache schema
+            self._schema = th.PropertiesList(*properties).to_dict()
+
+        return self._schema
+
     def get_records(
-        self,
-        context: Context | None,
+            self,
+            context: Context | None
     ) -> t.Iterable[dict]:
         """Return a generator of record-type dictionary objects.
 
         The optional `context` argument is used to identify a specific slice of the
         stream if partitioning is required for the stream. Most implementations do not
-        require partitioning and should ignore the `context` argument.
+        require partitioning and should ignore the `context` argument."""
 
-        Args:
-            context: Stream partition or context dictionary.
+        yield dict(zip(self.header, self.file.readline()))
 
-        Raises:
-            NotImplementedError: If the implementation is TODO
-        """
-        # TODO: Write logic to extract data from the upstream source.
-        # records = mysource.getall()  # noqa: ERA001
-        # for record in records:
-        #     yield record.to_dict()  # noqa: ERA001
-        errmsg = "The method is not yet implemented (TODO)"
-        raise NotImplementedError(errmsg)
+
--- tap_file/tap.py
+++ tap_file/tap.py
@@ -2,11 +2,11 @@
 
 from __future__ import annotations
 
-from singer_sdk import Tap
+from singer_sdk import Tap, Stream
 from singer_sdk import typing as th  # JSON schema typing helpers
 
 # TODO: Import your custom stream types here:
-from tap_file import streams
+from tap_file.client import CSVStream
 
 
 class TapFile(Tap):
@@ -14,40 +14,68 @@ class TapFile(Tap):
 
     name = "tap-file"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
-        th.Property(
-            "azure_storage_account_connection_string",
-            th.StringType(nullable=False),
-            required=False,
-            secret=True,  # Flag config as protected.
-            title="Azure Storage Account Connection String",
-            description="The connection string for the Azure Storage Account",
+        th.Property("files",
+                    th.ArrayType(
+                        th.ObjectType(
+                            th.Property("name",
+                                        th.StringType(nullable=False),
+                                        required=True,
+                                        title="Stream name",
+                                        description="Name used to name the stream when passed to the target."
+                            ),
+                            th.Property("url", 
+                                        th.StringType(nullable=False), 
+                                        required=True, 
+                                        title="URL", 
+                                        description="The path to the file(s).", 
+                                        examples=["local://mydir/myfile.csv", "https://www.mysite.com/myfile.csv", "azure://container/mydir/myfile.csv"]
+                            ),
+                            th.Property("format",
+                                        th.StringType(allowed_values=["csv", "parquet"],
+                                                      examples=["csv", "parquet"]
+                                        )
+                            )
+                        )
+                    )
         ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType(nullable=False), nullable=False),
-            required=True,
-            title="Project IDs",
-            description="Project IDs to replicate",
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType(nullable=True),
-            description="The earliest record date to sync",
-        ),
+        
+        th.Property("provider",
+                    th.ObjectType(
+                        th.Property("name",
+                                    th.StringType(allowed_values=["local", "http", "https", "azureblobstorage"],
+                                                    examples=["https", "azureblobstorage"]
+                                    )
+                        ),
+                        th.Property("azureblobstorage_connection_string",
+                                    th.StringType(nullable=False),
+                                    title="Azure Blob Storage connection string",
+                                    description="The connection string to reach and authenticate with the Azure Blob Storage service.")
+                    )
+        )
     ).to_dict()
 
-    def discover_streams(self) -> list[streams.FileStream]:
+    def discover_streams(self) -> list[Stream]:
         """Return a list of discovered streams.
 
         Returns:
             A list of discovered streams.
         """
-        return [
-            streams.GroupsStream(self),
-            streams.UsersStream(self),
-        ]
+
+        streams = []
+        for file in self.config.get('files'):
+            match file['format']:
+                case 'csv':
+                    streams.append(
+                        CSVStream(
+                            tap=self,
+                            name=file['name'],
+                            file_config=file,
+                            provider_config=self.config.get('provider')
+                        )
+                    ) 
+        return streams
+
 
 
 if __name__ == "__main__":
